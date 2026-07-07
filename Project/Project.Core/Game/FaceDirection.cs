84c4c92f167e9fc1924319ab3302163db59690ca
Patcharapakorn Norlar <Patcharapakorn_n@cmu.ac.th>
2026-07-07 16:10:47 +0700

    First

---
+++ Project/Project.Core/Game/FaceDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Core
+{
+    /// <summary>
+    /// Facing direction along the X axis.
+    /// </summary>
+    enum FaceDirection
+    {
+        Left = -1,
+        Right = 1,
+    }
+}
